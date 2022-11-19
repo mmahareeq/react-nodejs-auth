b464a9262d2a7fd5e6a3ef8a5bf1847f3fa5e0fe
mariam.mahareeq <mariam.mahareeq@transcend.ps>
2022-11-19 23:25:39 +0300

    implement back end

--- nodejs-auth/server.js
+++ nodejs-auth/server.js
@@ -3,13 +3,27 @@ const app = express()
 
 const path = require('path')
 const cors = require('cors');
+const corOptions = require('./config/corsOptions');
+const credential = require('./middleware/credentials');
 const mongoose = require('mongoose')
+const registerRouter = require('./routes/registers');
 const PORT = process.env.PORT || 3500;
+
+app.use(credential)
+
+app.use(cors(corOptions))
+
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.json());
 
 mongoose.connect('mongodb://localhost:27017/auth-user').
 then(console.log('connection'))
 .catch((err)=>{console.log(err)});
 
+
+
+app.use('/register', registerRouter);
 app.listen(PORT, ()=>{
     console.log(`server running on port ${PORT}`);
 })
